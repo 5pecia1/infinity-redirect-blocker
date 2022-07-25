236d6a65be771defdae966a3a9d838b816e176e6
5pecia1 <pdpxpd@gmail.com>
2022-07-25 22:16:11 +0900

    init project

---
+++ main.ts
@@ -0,0 +1,36 @@
+import { decode } from "https://deno.land/std@0.140.0/encoding/base64.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const router = new Router();
+router.get("/decode/:url", async (ctx) => {
+    const url = new TextDecoder().decode(decode(ctx.params.url));
+
+    let resp = await fetch(url, {
+        redirect: "manual",
+    });
+
+    const cookie = resp.headers.get("set-cookie");
+    const location = resp.headers.get("location");
+
+    resp = await fetch(location ?? url, {
+        headers: {
+            "Cookie": cookie ?? "",
+        },
+    });
+
+    ctx.response.body = await resp.text()
+});
+
+const app = new Application();
+app.use(async (ctx, next) => {
+    console.log(`${ctx.request.method} ${ctx.request.url}`);
+    await next();
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener(
+    "listen",
+    (e) => console.log("Listening on http://localhost:8080"),
+);
+await app.listen({ port: 8080 });
